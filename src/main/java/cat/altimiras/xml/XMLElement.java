a584cc524794b7925bb169f58969a6be0440e9d6
faltimiras <ferran@altimiras.cat>
2017-04-03 21:39:43 +0200

    clone base obj. performance improvement

---
+++ src/main/java/cat/altimiras/xml/XMLElement.java
@@ -0,0 +1,9 @@
+package cat.altimiras.xml;
+
+public abstract class XMLElement implements Cloneable {
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
